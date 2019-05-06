2112059df92e9b8a976ca4487efea5561be366d6
munemu82 <munemu82@gmail.com>
2019-05-06 19:01:10 +1000

    update code

--- lbp_feature_extraction.py
+++ lbp_feature_extraction.py
@@ -7,25 +7,27 @@ import cv2
 import os
 
 # construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-t", "--training", required=True,
-                help="path to the training images")
+#ap = argparse.ArgumentParser()
+#ap.add_argument("-t", "--training", required=True,
+                #help="path to the training images")
 #ap.add_argument("-e", "--testing", required=True,
                # help="path to the tesitng images")
-args = vars(ap.parse_args())
+#args = vars(ap.parse_args())
 
 # initialize the local binary patterns descriptor along with
 # the data and label lists
-desc = LocalBinaryPatterns(16, 8)
+desc = LocalBinaryPatterns(1000, 3)
 data = []
 labels = []
-
+path = 'C:/Users/admin/Documents/MastersThesis/ImageProcessingAndObjectRecognition/processed_images/Zebra'
 # loop over the training images
-for imagePath in paths.list_images(args["training"]):
+for imagePath in paths.list_images(path):
     # load the image, convert it to grayscale, and describe it
-    image = cv2.imread(imagePath)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    hist = desc.describe(gray)
+    image = cv2.imread(imagePath, cv2.IMREAD_GRAYSCALE)
+    print(image.shape)
+    #gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    #print(gray.shape)
+    hist = desc.describe(image)
     print(hist.shape)
 
     # extract the label from the image path, then update the
@@ -33,22 +35,22 @@ for imagePath in paths.list_images(args["training"]):
     labels.append(imagePath.split(os.path.sep)[-2])
     data.append(hist)
 print(len(data))
-# train a Linear SVM on the data
-print(labels)
-model = LinearSVC(C=100.0, random_state=42)
-model.fit(data, labels)
-
-# loop over the testing images
-for imagePath in paths.list_images(args["testing"]):
-    # load the image, convert it to grayscale, describe it,
-    # and classify it
-    image = cv2.imread(imagePath)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    hist = desc.describe(gray)
-    prediction = model.predict(hist.reshape(1, -1))
-
-    # display the image and the prediction
-    cv2.putText(image, prediction[0], (10, 30), cv2.FONT_HERSHEY_SIMPLEX,
-                1.0, (0, 0, 255), 3)
-    cv2.imshow("Image", image)
-    cv2.waitKey(0)
+# # train a Linear SVM on the data
+# print(labels)
+# model = LinearSVC(C=100.0, random_state=42)
+# model.fit(data, labels)
+#
+# # loop over the testing images
+# for imagePath in paths.list_images(args["testing"]):
+#     # load the image, convert it to grayscale, describe it,
+#     # and classify it
+#     image = cv2.imread(imagePath)
+#     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+#     hist = desc.describe(gray)
+#     prediction = model.predict(hist.reshape(1, -1))
+#
+#     # display the image and the prediction
+#     cv2.putText(image, prediction[0], (10, 30), cv2.FONT_HERSHEY_SIMPLEX,
+#                 1.0, (0, 0, 255), 3)
+#     cv2.imshow("Image", image)
+#     cv2.waitKey(0)
